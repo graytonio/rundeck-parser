83ceea0b05860f37ee7a32327c10673ad90b1f62
Grayton Ward <graytonio.ward@gmail.com>
2022-06-29 23:32:27 -0400

    Added github testing action

--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ type lineStripTest struct {
 var lineStripTests = []lineStripTest{
 	{input: "15:41:44 [rundeck@zt-ops-0 2][NORMAL] TASK [APPLY SQL - FAIL if SQL failed] ******************************************", expected: "TASK [APPLY SQL - FAIL if SQL failed] ******************************************"},
 	{input: "15:48:00 [rundeck@zt-ops-0 2][NORMAL] fatal: [la-grant-so-prod-1]: FAILED! => {\"changed\": true, \"rc\": 4}", expected: "fatal: [la-grant-so-prod-1]: FAILED! => {\"changed\": true, \"rc\": 4}"},
+	{input: "15:30:20 [rundeck@zt-ops-0 1][ERROR] [WARNING]: Found both group and host with same name: zt-testing-6", expected: "[WARNING]: Found both group and host with same name: zt-testing-6"},
 }
 
 func TestStripLineInfo(t *testing.T) {
