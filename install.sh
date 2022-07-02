e949c5fb82fec50b2512ef66eac00742196bb9f8
Grayton Ward <graytonio.ward@gmail.com>
2022-07-02 17:06:53 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
 install_path=/usr/bin/rundeck-parser
 current_version_link=https://github.com/graytonio/rundeck-parser/releases/download/v0.2/rundeck-parser-v0.2-linux-amd64.tar.gz
 tmp_download_location=/tmp/rundeck-parser.tar.gz
@@ -9,7 +14,7 @@ echo "Checking for existing install"
 if [ -f "$install_path" ]; then
     echo "Found existing installation"
     echo "Removing existing installation"
-    rm $install_path
+    sudo rm $install_path
 fi
 
 echo "Downloading latest version"
@@ -17,5 +22,5 @@ rm /tmp/rundeck-parser.tar.gz
 curl -LJ $current_version_link -o $tmp_download_location
 
 echo "Installing rundeck-parser"
-sudo tar -xvfz $tmp_download_location -C /usr/bin
+sudo tar -xvzf $tmp_download_location -C /usr/bin
 echo "rundeck-parser installed"
